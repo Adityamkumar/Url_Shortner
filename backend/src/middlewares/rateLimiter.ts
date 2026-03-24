3ac0a4a445eb597e77e03839db4b0fcf806e1f75
Adityamkumar <adityasharma841460@gmail.com>
2026-03-24 15:21:08 +0530

    feat: implement Redis caching for fast redirects and IP-based rate limiting
    - added Redis cache layer to reduce DB reads
    - implemented IP-based rate limiter middleware
    - ensured visitCount updates on both cache hit and DB access
    - improved overall performance and request handling

---
+++ backend/src/middlewares/rateLimiter.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, NextFunction } from "express";
+import redisClient from "../utils/redisClient.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+
+export const rateLimiter = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const ip = req.ip || req.socket.remoteAddress || "unknown";
+    const key = `rate_limit${ip}`;
+
+    const count = await redisClient.incr(key);
+
+    if (count === 1) {
+      await redisClient.expire(key, 60);
+    }
+
+    if (count > 5) {
+      throw new ApiError(429, "Too many requests, try again later");
+    }
+    next();
+  },
+);
--- backend/src/routes/url.route.ts
+++ backend/src/routes/url.route.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express'
-import { generateShortId, redirectToOriginalUrl } from '../controllers/url.controller.js'
-const router = Router()
+import { Router } from "express";
+import {
+  generateShortId,
+  redirectToOriginalUrl,
+} from "../controllers/url.controller.js";
+import { rateLimiter } from "../middlewares/rateLimiter.js";
+const router = Router();
 
-router.post('/shortId', generateShortId)
-router.get('/:shortId', redirectToOriginalUrl)
+router.post("/shortId", rateLimiter, generateShortId);
+router.get("/:shortId", redirectToOriginalUrl);
 
-export default router
+export default router;
