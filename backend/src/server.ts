2a0b4b67490d9130ecbba620585838c5e63c6dad
Adityamkumar <adityasharma841460@gmail.com>
2026-04-22 16:35:51 +0530

    feat: add Docker support with multi-stage build and deployment setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./db/db.js";
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT) || 6000;
+
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
+  app.listen(PORT,"0.0.0.0", () => console.log(`Server running on PORT: ${PORT}`));
 };
 
 startServer();
